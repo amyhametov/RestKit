81ae2253224ff3591d178a6270415574dcf94b9b
Ruslan Kurmakaev <rkurmakaev@fun.co>
2018-06-04 10:37:24 +0300

    Fix comment

--- Code/ObjectMapping/RKMappingOperation.m
+++ Code/ObjectMapping/RKMappingOperation.m
@@ -559,7 +559,7 @@ static NSArray *RKInsertInMetadataList(NSArray *list, id metadata1, id metadata2
         if ([destinationObject respondsToSelector:@selector(validateValue:forKeyPath:error:)]) {
             NSError *validationError;
             
-            // Default implementation of validateValue:forKey:error: on iOS 11 is causes memory leaks.
+            // Default implementation of validateValue:forKey:error: on iOS 11 causes memory leaks.
             // http://www.openradar.me/37115134
             if (@available(iOS 11, *) && ![self checkIfObject:destinationObject overridesSelector:@selector(validateValue:forKey:error:)]) {
                 success = YES;
